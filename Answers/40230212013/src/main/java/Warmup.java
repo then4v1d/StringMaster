d1dece4c11b8d2ef595e4eac55b74b6b60607076
The Navid <thenavid2005@gmail.com>
2024-03-03 22:44:47 +0330

    wordFinder

---
+++ Answers/40230212013/src/main/java/Warmup.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Warmup {
+   /**
+     * Goal : Simple Introduction To Strings
+     * In the first function, your inputs are a number and a sentence
+     * @return is the number th word of the sentence
+     */
+    public String wordFinder(String sentence, int number) {
+        String[] araye = sentence.split(" ");
+        if(number > araye.length-1)
+        {
+            return " Number = "+String.valueOf(number)+" is out Of Bound";
+        }
+        return araye[number-1];
+    }
+
+    /**
+     * Goal : Basic introduction to Strings & using foreach
+     * @param number        is in String type
+     * @param searchForEven is a boolean entry
+     * @return if searchForEven is true ? return the number of even numbers : return the number of odd numbers
+     */
+    public int oddEvenCounter(String number, boolean searchForEven) {
+        return -1;
+    }
+
+    /**
+     * @param wordA --> first word
+     * @param wordB --> second word
+     * @return The word that is first in alphabet column
+     */
+    public String firstWord(String wordA, String wordB) {
+        return null;
+    }
+}
